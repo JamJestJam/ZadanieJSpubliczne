23268edf0c1d90ee5ffd23fd1976afa17229c161
JarekMyjak <jarekmyjak99@gmail.com>
2020-11-26 14:27:47 +0100

    added static gallery

---
+++ main.js
@@ -0,0 +1,14 @@
+// let gallery = document.querySelector("#dynamicGallery")
+
+// let imagesArray = [];
+
+// for (let i = 0; i < 5; i++) {
+//     const img = document.createElement("img");
+//     img.src = `https://picsum.photos/seed/${i+1}/300/200`
+//     imagesArray[i] = img;
+// }
+
+// imagesArray.map((img)=>{
+//     gallery.appendChild(img)
+// })
+// console.log(imagesArray )
