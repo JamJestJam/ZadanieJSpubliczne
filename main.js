dba31a9da498fe72bcbc6a62fc63dc2a115b01b4
JamJestJam <marekti012@gmail.com>
2020-11-26 14:06:02 +0100

    Galeria 5 zdjęc

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const doc = document.querySelector("#Marek");
+const src_s = ["", 
+                "https://picsum.photos/id/236/200/300",
+                "https://picsum.photos/id/235/200/300",
+                "https://picsum.photos/id/234/200/300",
+                "https://picsum.photos/id/233/200/300",
+            ];
 
-for (let i = 0; i < 10;i++) {
+for (let index = 0; index < src_s.length; index++) {
+    const element = src_s[index];
+    
     const tmp = new Image();
-    tmp.src = "";
+    tmp.src = element;
+
+    doc.appendChild(tmp);
 }
