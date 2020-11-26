7f3b303f44f8de8a4fad26b312446e316fdfb32a
JarekMyjak <jarekmyjak99@gmail.com>
2020-11-26 15:00:34 +0100

    added dynamic galery

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-// let gallery = document.querySelector("#dynamicGallery")
+let gallery = document.querySelector("#dynamicGallery")
 
-// let imagesArray = [];
-
-// for (let i = 0; i < 5; i++) {
-//     const img = document.createElement("img");
-//     img.src = `https://picsum.photos/seed/${i+1}/300/200`
-//     imagesArray[i] = img;
-// }
-
-// imagesArray.map((img)=>{
-//     gallery.appendChild(img)
-// })
-// console.log(imagesArray )
+let seed = 1;
+const loadImage = (num) => {
+    if (gallery.childElementCount < num) {
+        const img = document.createElement("img")
+        img.src = `https://picsum.photos/seed/${seed++}/300/200`
+        img.addEventListener("load", (e)=>{
+            gallery.appendChild(img)
+            loadImage(num);
+        })
+    }
+}
+loadImage(5)
